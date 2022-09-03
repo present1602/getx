0a1585e64342de9447913b0e1f0e2f76e17a3021
cy <present1306@naver.com>
2022-09-04 02:50:40 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bottom_navigation_flutter/pages/dashboard/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyDashBoard(),
+    );
+  }
+}
